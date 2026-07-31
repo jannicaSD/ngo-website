cd564dea248844072c60314489f02842589207da
jannicaSD <bc210423691jsa@vu.edu.pk>
2026-07-31 15:49:11 -0400

    material changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../style/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo1.png" type="image/png" />
+        <title>Moments of Hope</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
